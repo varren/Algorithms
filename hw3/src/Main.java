d309600b028c64c951a3216ff078da681b546410
Varren <varren@gmail.com>
2013-08-04 10:27:44 +0400

    hw3 done

--- hw3/src/Main.java
+++ hw3/src/Main.java
@@ -4,17 +4,53 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
+    private static final String filename = System.getProperty("user.dir")+ "\\hw3\\src" + "\\kargerMinCut.txt";
     private static HashMap<Integer,ArrayList<Integer>> data = new HashMap<Integer, ArrayList<Integer>>();
+
 
     public static void main (String[] args){
         loadFile();
         System.out.println(data.toString());
 
+        int solution = findMinCut(data);
+        System.out.println(solution);
     }
-    private static final String filename = System.getProperty("user.dir")+ "\\hw3\\src" + "\\kargerMinCut.txt";
+    public static int findMinCut(HashMap<Integer,ArrayList<Integer>> data) {
+
+        int numOfItter = 100;
+        RandomizedContractionAlgorithm find = new RandomizedContractionAlgorithm();
+        int minCut = data.size();
+
+        for (int i = 0; i < numOfItter; i++){
+            HashMap<Integer,ArrayList<Integer>> newData = clone(data);
+            int currentVal = find.contractionAlgorithm(newData);
+            if(currentVal<minCut)
+                minCut=currentVal;
+        }
+        return minCut;
+
+    }
+
+    public static HashMap<Integer, ArrayList<Integer>> clone( HashMap<Integer, ArrayList<Integer>> m){
+        final HashMap<Integer, ArrayList<Integer>> result = new HashMap<Integer, ArrayList<Integer>>();
+
+        for (final Map.Entry<Integer,ArrayList<Integer>> e : m.entrySet())
+        {
+            int key = e.getKey();
+            ArrayList<Integer> value = new ArrayList<Integer>();
+
+            for(int i : e.getValue())
+                value.add(i);
+
+            result.put(key, value);
+        }
+
+        return result;
+    }
 
     private static void loadFile(){
         try {
@@ -22,24 +58,27 @@ public class Main {
             BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
             while(true){
                 String line = br.readLine();
+
                 if(line == null)
                     break;
-                String [] newEntry = line.split("\\s");
-                int key =Integer.parseInt(newEntry[0]);
-                ArrayList<Integer> currentNodeConnections = new ArrayList<Integer>();
 
-
-                for(int i = 1;i < newEntry.length; i++){
-                    currentNodeConnections.add(Integer.parseInt(newEntry[i]));
-                }
-
-                data.put(key,currentNodeConnections);
-
-
-
+                addEntry(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void addEntry(String line) {
+        String [] newEntry = line.split("\\s");
+        int key =Integer.parseInt(newEntry[0]);
+        ArrayList<Integer> currentNodeConnections = new ArrayList<Integer>();
+
+
+        for(int i = 1;i < newEntry.length; i++){
+            currentNodeConnections.add(Integer.parseInt(newEntry[i]));
+        }
+
+        data.put(key,currentNodeConnections);
     }
 }
--- hw3/src/RandomizedContractionAlgorithm.java
+++ hw3/src/RandomizedContractionAlgorithm.java
@@ -1,2 +1,62 @@
+import java.util.*;
+
 public class RandomizedContractionAlgorithm {
+   /*
+    * Karger's algorithm / Contraction algorithm (minimum cut problem)
+    * 1) while there are > 2 vertices
+    * 2) pick the remaining edge(u,v) uniformly at random
+    * 3) merge u and v in a single vertex
+    * 4) remove self loop
+    * 5) return cut represented by 2 final vertices
+    */
+
+    public int contractionAlgorithm(HashMap<Integer,ArrayList<Integer>> data){
+
+        int randomVertex = 0;
+
+        while(data.size() > 2){
+            randomVertex = selectRandomVertex(data);
+            
+            ArrayList<Integer> randomEdges = data.get(randomVertex);
+
+            int edgeToMergeWith = selectRandomEdge(randomEdges);
+
+            if(edgeToMergeWith != randomVertex)
+                merge(randomVertex, edgeToMergeWith,data);
+        }
+
+        return data.get(randomVertex).size();
+    }
+
+    private void merge(int oneID, int twoID, HashMap<Integer, ArrayList<Integer>> data) {
+        ArrayList<Integer> oneEdges = data.get(oneID);
+        ArrayList<Integer> twoEdges = data.get(twoID);
+
+        oneEdges.removeAll(Collections.singleton(twoID));
+        twoEdges.removeAll(Collections.singleton(oneID));
+
+        for(int i: twoEdges){
+            ArrayList<Integer> connectedNode = data.get(i);
+            connectedNode.remove(connectedNode.indexOf(twoID));
+            connectedNode.add(oneID);
+
+            oneEdges.add(i);
+        }
+
+        data.remove(twoID);
+    }
+
+    private int selectRandomEdge(ArrayList<Integer> edges) {
+        return selectRandom(edges);
+    }
+
+    private int selectRandomVertex(HashMap<Integer,ArrayList<Integer>> data){
+        return selectRandom(new ArrayList<Integer>(data.keySet()));
+    }
+
+    private int selectRandom(ArrayList<Integer> edges) {
+        Random random = new Random(Double.doubleToLongBits(Math.random()));
+        return edges.get(random.nextInt(edges.size()));
+    }
+
 }
